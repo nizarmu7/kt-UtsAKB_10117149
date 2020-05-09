19c4e7dc880b03e2d77b0dcded9b835772abbfd5
Your Name <nizarmu7@gmail.com>
2020-05-09 17:40:27 +0700

    Penambahan "Bottom Navigation"

--- app/src/main/java/com/akb/uts_10117149/HomeActivity.kt
+++ app/src/main/java/com/akb/uts_10117149/HomeActivity.kt
@@ -1,29 +1,86 @@
 package com.akb.uts_10117149
 
 /*
-Tanggal Pengerjaan : Jum'at, 08 Mei 2020
+Tanggal Pengerjaan : 08 Mei 2020 - 09 Mei 2020
 Nim : 10117149
 Nama : M Nizar Miftahul Ulya
 Kelas : IF-4
  */
 
-import androidx.appcompat.app.AppCompatActivity
+import PreferenceHelper
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                val fragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_daily -> {
+                val fragment = DailyFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_media -> {
+                val fragment = MediaFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                val fragment = ProfileFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+    private var preferenceHelper: PreferenceHelper? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        nav_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        preferenceHelper = PreferenceHelper(this)
+
+        if (savedInstanceState == null) {
+            val fragment = HomeFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                .commit()
+        }
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) hideSystemUI()
     }
+
     private fun hideSystemUI() {
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
+
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
 }
+
+
+
+
+
+
+
+
